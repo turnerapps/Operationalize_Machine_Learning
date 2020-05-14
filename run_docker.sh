5d8c2e1dcf22ce953a8f86954fa3d65f7a835757
Sam Turner <skwirrley5@gmail.com>
2020-05-14 02:11:50 -0400

    complete run_docker edit

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t prediction .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -it --rm --name prediction -p 8000:80 prediction
